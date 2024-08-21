609d75714339373f3c8edb19ba2e6bc2a3b90614
rafael murtz <salinhastropa@gmail.com>
2024-08-21 20:50:51 -0300

    Create exercicio3 (1).py
    3

---
+++ exercicio3 (1).py
@@ -0,0 +1,9 @@
+def soma_tres_numeros():
+    """Recebe três números do usuário e calcula a soma deles, exibindo o resultado."""
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    num3 = float(input("Digite o terceiro número: "))
+    soma = num1 + num2 + num3
+    print("A soma dos três números é:", soma)
+
+soma_tres_numeros()
