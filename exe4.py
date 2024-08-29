a546e44a1b1b36f229e66aa2738a1d6a60bced32
lucaswillian4 <salinhastropa@gmail.com>
2024-08-28 21:00:24 -0300

    Create exe4.py

---
+++ exe4.py
@@ -0,0 +1,6 @@
+n1 = int(input("digite o primeiro numero "))
+n2 = int(input("digite o segundo numero "))
+if (n1>n2):
+  print("o primeiro numero é maior ")
+else:
+  print(" o segundo numero é maior ")
