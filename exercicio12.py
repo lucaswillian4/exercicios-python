3b7e622356a91eee4c05113533a01448df9c7c26
lucaswillian4 <salinhastropa@gmail.com>
2024-09-04 21:07:04 -0300

    tg

---
+++ exercicio12.py
@@ -0,0 +1,5 @@
+numero = int(input("digite um numero: "))
+if numero>0:
+  print ("seu numero é positivo: ")
+else:
+  print ("seu numero é negativo: ")
---
+++ exercicio13.py
@@ -0,0 +1,3 @@
+metros = int(input("digite quantos metros quer converter: "))
+cent = 100*metros
+print (f"seu resultado é de {cent} centimrtros ")
