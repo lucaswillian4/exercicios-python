92c0ba74c4287038f067479316578408c4a6c47b
lucaswillian4 <salinhastropa@gmail.com>
2024-09-10 21:01:29 -0300

    exerc 14 e 15

---
+++ exercicio14.py
@@ -0,0 +1,7 @@
+gene = input("digite seu genero")[0]
+if gene == "M" or gene == "m":
+ print ("seu genero é masculino")
+elif gene == "F" or gene == "f":
+ print ("seu genero é feminino")
+else:
+ print ("genero nao identificado")
---
+++ exercicio15.py
@@ -0,0 +1,3 @@
+sala = int(input("qual o seu salario"))
+nvsa = sala*1.25
+print (f"seu salario teve um aumento de 25%: {nvsa}")
